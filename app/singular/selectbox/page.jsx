f989ee283f41c733023c20ef4242aecf87b0986e
ittehadrahman <95122519+ittehadrahman@users.noreply.github.com>
2024-06-05 15:26:49 +0600

    feature: add custom select box

---
+++ app/singular/selectbox/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import CustomSelectBox from './components/CustomSelectBox'
+
+const SelectBoxPage = () => {
+  return (
+    <div className='bg-white rouonded-lg mx-4 p-4'>
+        <h1>This is SelectBoxPage</h1>
+        <br />
+        <br />
+        <CustomSelectBox />
+    </div>
+  )
+}
+
+export default SelectBoxPage
